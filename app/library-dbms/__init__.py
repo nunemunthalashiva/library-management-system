3b4e7fa43d7b65be6b8870fe02fb2b18d7e6943b
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-04-18 20:51:46 +0530

    deleted unnecessary files

--- app/library-dbms/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask , render_template, request ,redirect , url_for ,session
-from flask_mysqldb import MySQL
-import MySQLdb.cursors
-import library_dbms.routes
-
-app = Flask(__name__)
-mysql = MySQL(app)
-app.config.from_pyfile('config.py')
