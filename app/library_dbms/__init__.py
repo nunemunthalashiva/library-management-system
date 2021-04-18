a9d0fcb05d4f64032e5d3eb37c53c9ce6e5f956f
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-04-18 21:12:32 +0530

    todays final commit and push

--- app/library_dbms/__init__.py
+++ app/library_dbms/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask , render_template, request ,redirect , url_for ,session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-import library_dbms.routes
+
 app = Flask(__name__)
 mysql = MySQL(app)
+import library_dbms.routes
 app.config.from_pyfile('config.py')
