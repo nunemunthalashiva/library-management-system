1e2aa8b77813a92c3d2841232ae12b8a95bcea8a
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-04-18 17:08:04 +0530

    added functions login adduser signin logout

--- app/library-dbms/__init__.py
+++ app/library-dbms/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask , render_template, request ,redirect , url_for ,session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
-
+import routes
 app = Flask(__name__)
+mysql = MySQL(app)
 app.config.from_pyfile('config.py')
---
+++ app/runserver.py
@@ -0,0 +1,3 @@
+from library-dbms import app
+
+app.run(debug=True)
