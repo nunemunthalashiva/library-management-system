fdc6246ce0d38075c25ab333015c0801f4791c06
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-04-18 20:50:48 +0530

    added full path

--- app/library-dbms/__init__.py
+++ app/library-dbms/__init__.py
@@ -1,7 +1,7 @@
-import routes
 from flask import Flask , render_template, request ,redirect , url_for ,session
 from flask_mysqldb import MySQL
 import MySQLdb.cursors
+import library_dbms.routes
 
 app = Flask(__name__)
 mysql = MySQL(app)
