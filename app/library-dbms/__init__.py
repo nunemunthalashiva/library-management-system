1984fbaa6f56755593ffc6f83d004059ca175e38
nunemunthala shiva <nunemunthalashiva12@gmail.com>
2021-04-18 15:29:52 +0530

    Added comfig file and .env file

---
+++ app/library-dbms/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask , render_template, request ,redirect , url_for ,session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
---
+++ app/library-dbms/config.py
@@ -0,0 +1,11 @@
+""" Flask configuration """
+from os import environ , path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+TESTING = True
+DEBUG = True
+FLASK_ENV = 'development'
+SECRET_KEY = environ.get('SECRET_KEY')
