4461bd5356d4c09c62fe07f8736a50041c197572
Kethavath jagadeesh <70793776+jagadeesh-kethavath-23@users.noreply.github.com>
2021-04-21 21:37:08 +0530

    Update routes.py

--- app/library_dbms/routes.py
+++ app/library_dbms/routes.py
@@ -408,8 +408,11 @@ def check_review():
             if valid:
                 cursor.execute("SELECT * FROM review WHERE ISBN_number= (%s)",(ISBN_number,))
                 reviews = cursor.fetchall()
-                msg = " review of this book "
-                return render_template('check_review_2.html',reviews=reviews,msg=msg)
+                if reviews:
+                    msg = " review of this book "
+                    return render_template('check_review_2.html',reviews=reviews,msg=msg)
+                msg = "this is not reviewed at all"
+                return render_template('check_review.html',msg=msg)
             msg = 'invalid isbn number/no such books present in library'
             return render_template('check_review.html',msg=msg)
         msg = 'please enter isbn number'
